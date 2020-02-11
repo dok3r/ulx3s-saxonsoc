3858181d9abfe605c2a5faeadb965794ac77a7df
Vlatko Kosturjak <kost@linux.hr>
2020-02-11 21:00:28 +0100

    Find free losetup devices automatically

--- scripts/createimage.sh
+++ scripts/createimage.sh
@@ -2,8 +2,6 @@
 # create image from rootfs.tar.gz
 
 # Use losetup -f to find free `loop devices
-LOOP_DEVICE1=/dev/loop20
-LOOP_DEVICE2=/dev/loop21
 
 # Create a 32MB image file
 rm image
@@ -12,13 +10,11 @@ dd if=/dev/zero of=image bs=1M count=32
 # Create 1 Linux partition
 fdisk image <fdisk.cmd
 
-# Detach loop devices
-sudo losetup -d ${LOOP_DEVICE1}
-sudo losetup -d ${LOOP_DEVICE2}
-
+LOOP_DEVICE1=`losetup -f`
 # Set loop device 1 to the whole file
 sudo losetup ${LOOP_DEVICE1} image
 
+LOOP_DEVICE2=`losetup -f`
 # Set loop device 2 to the start of partition
 sudo losetup -o $((512*2048)) ${LOOP_DEVICE2} ${LOOP_DEVICE1}
 
